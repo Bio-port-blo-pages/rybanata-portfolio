964d1169135ae3d60bdc746e317a437eb6d93a1f
Lev Sergeyev <lev.sergeyev@bustec.com>
2023-11-18 17:13:50 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://www.rybanata.art',
 	// base: '/rybanata-portfolio',
-	vite: {
-		resolve: {
-		  alias: {
-			 "svgo": import.meta.env.PROD ? "svgo/dist/svgo.browser.js" : "svgo"
-		  }
-		}
-	}
 });
-
