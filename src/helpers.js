86cbd64c3b20fa8874507d9e922a425ffb2a3c5f
Lev Sergeyev <lev.sergeyev@bustec.com>
2023-11-19 16:53:08 +0100

    update

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+import fsp from 'fs/promises';
+import * as fs from 'fs';
+import probe from 'probe-image-size';
+
+export async function getImages(dir) {
+  try {
+    const files = await fsp.readdir(dir);
+
+    return await Promise.all( files.map(async (files) => {
+      const label = files;
+      let href = dir + files;
+      let data = fs.readFileSync(href);
+      const size = await probe.sync(data);
+
+      // optional fix for unicode
+      href = await encodeURI(href);
+      return { label, href, size };
+    }));
+  } catch (err) {
+    console.error('Error occurred while reading directory:', err)
+  }
+}
