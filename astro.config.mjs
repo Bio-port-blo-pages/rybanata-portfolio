49adf88d6d90f72d0c3b8744ee8025db06988b82
LS <sergeyev.lev@protonmail.com>
2026-05-29 11:41:59 +0200

    fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { Icon } from 'astro-icon/components';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.rybanata.art',
 	// base: '/rybanata-portfolio',
+	integrations: [Icon()],
 });
